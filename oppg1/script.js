7d84c6b5eec23788e8ff910a620ad8feacff948d
Denniz <dennizy@viken.no>
2022-10-26 12:59:39 +0200

    first commit

---
+++ oppg1/script.js
@@ -0,0 +1,23 @@
+var currentcolor = document.querySelector("#colortext")
+
+const setColor = document.querySelector("#setcolor").addEventListener("click", function() {
+    color = colorinput.value
+
+    document.body.style.backgroundColor = (color)
+    currentcolor.innerHTML = document.body.style.backgroundColor
+})
+
+
+
+
+const randomColor = document.querySelector("#randomcolor").addEventListener('click', function (){
+    const colorList = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey"]
+    var randomNumber = Math.round(Math.random() * 25);
+   
+   
+   
+    document.body.style.backgroundColor = (colorList[randomNumber])
+    currentcolor.innerHTML = document.body.style.backgroundColor
+}
+)
+
